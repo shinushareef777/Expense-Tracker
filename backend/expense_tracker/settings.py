57c6f6f8f4ddeef1f2f2b00d74f945eb14190f6d
shinusAlokin <shinus@alokin.in>
2024-04-26 21:00:48 +0530

    dockerize

--- backend/expense_tracker/settings.py
+++ backend/expense_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&m8p=+x9$tz9b5$6flb3qa0wn7629**&cw1xwqvwog6!8w+-b%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.0"]
 
 
 # Application definition
